a0eac2168be5ea175e6bb085307484964dfcf790
Jonatan Rodriguez <jrperdomoz@gmail.com>
2016-09-22 12:23:32 +0300

    se actualiza el hobbit

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,36 @@
+import { exec } from 'child_process'
 import bluebird from 'bluebird'
 import fs from 'fs'
+import mkdirp from 'mkdir-p'
 import path from 'path'
 import structure from './structure'
-import mkdirp from 'mkdir-p'
-
-const ROOT_DIR = path.join(__dirname)
-const TEMPLATES_DIR = path.join(ROOT_DIR, 'templates')
-const FOLDERS = ['controllers', 'models', 'helpers', 'middlewares']
 const mkdirAsync = bluebird.promisify(mkdirp)
 const readFile = bluebird.promisify(fs.readFile)
 const writeFile = bluebird.promisify(fs.writeFile)
+
+const FOLDERS = ['controllers', 'models', 'helpers', 'middlewares']
+const ROOT_DIR = path.join(__dirname)
+const TEMPLATES_DIR = path.join(ROOT_DIR, 'templates')
+
+const execAsync = (command, dir) => {
+  console.log(dir)
+  process.chdir(dir)
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) return reject(err)
+      resolve(stdout)
+    })
+  })
+}
+
+const createPackageJson = (dir) => {
+  execAsync('npm init -y', dir)
+  .then(
+    execAsync('npm i -S body-parser bcrypt-nodejs bcrypt babel-register babel-preset-stage-0 babel-preset-es2016 babel-preset-es2015 boom chalk express jsonwebtoken mongoose bluebird validator slug morgan cors cookie-parser', dir)
+    .then(execAsync('npm i -D babel-eslint nodemon standard', dir))
+  )
+  .catch((err) => console.log(err))
+}
 
 const createFiles = (dir) => {
   const fromDest = []
@@ -25,6 +46,7 @@ const createFiles = (dir) => {
     }
   }
   bluebird.all(fromDest.map((file) => readFile(file._from).then((content) => writeFile(`${file.to}`, content).catch((err) => console.log(err)))))
+  .then(() => createPackageJson(dir))
 }
 
 export const buildFoldersAsync = (base) =>
