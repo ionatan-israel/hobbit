c97a2bcf84e69e362417f1aa58d4290d74254df0
Jonatan Rodriguez <jrperdomoz@gmail.com>
2016-09-26 05:28:19 +0300

    Update routes (automatic routes list:create)

--- src/templates/src/router.js
+++ src/templates/src/router.js
@@ -3,11 +3,17 @@ import { Router } from 'express'
 import ctrl from './controllers/'
 
 const router = Router()
-.get('users', ctrl.users.list)
-.post('users', ctrl.users.create)
-.post('login', ctrl.users.auth)
-.post('register', ctrl.users.register)
-.use(err)
-.use('*', notFound)
+.post('/auth', ctrl.users.auth)
+.post('/register', ctrl.users.register)
+
+Object.keys(ctrl).forEach((c) => {
+  Object.keys(ctrl[c]).forEach((m) => {
+    if (m === 'list') router.get(`/${c}`, ctrl[c].list)
+    if (m === 'create') router.post(`/${c}`, ctrl[c].create)
+  })
+})
+
+router.use('*', notFound)
+router.use(err)
 
 export default router
